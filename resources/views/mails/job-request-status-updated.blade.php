27e3fca347dab7d3bf44c68924cdf84aa0961996
Hussein Mohammed <hussein.mohammed@tiu.edu.iq>
2022-12-30 19:24:02 +0300

    imgs 4

--- resources/views/mails/job-request-status-updated.blade.php
+++ resources/views/mails/job-request-status-updated.blade.php
@@ -21,11 +21,8 @@
             <div class="max-w-xl p-8 text-center text-gray-800 bg-white shadow-xl lg:max-w-3xl rounded-3xl lg:p-12">
                 <h3 class="text-2xl">Dear {{ $request->user->name }}</h3>
                 <div class="flex justify-center">
-                    <img src="{{ asset('imgs/logo.png') }}" class="w-24 h-24 text-green-400" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                    <div stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                        d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76">
-                    </div>
+                    <img src="{{ asset('imgs/logo.png') }}" style="height: 100px; width:100px;">
+
                 </div><br><br>
 
                 <p>Congratulations! After reviewing your application for the {{ $request->job->title }} position at
@@ -61,11 +58,8 @@
             <div class="max-w-xl p-8 text-center text-gray-800 bg-white shadow-xl lg:max-w-3xl rounded-3xl lg:p-12">
                 <h3 class="text-2xl">Dear {{ $request->user->name }}</h3>
                 <div class="flex justify-center">
-                    <img src="{{ asset('imgs/logo.png') }}" class="w-24 h-24 text-green-400" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor" />
-                    <div stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                        d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76">
-                    </div>
+                    <img src="{{ asset('imgs/logo.png') }}" style="height: 100px; width:100px;">
+
                 </div><br><br>
 
                 <p>We would like to thank you for your application for the position {{ $request->job->title }} at
